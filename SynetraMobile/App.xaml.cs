6f53e0e29cb75e76d22707b763cf31dcb4b2f68f
Elvis OHIN <elvisohin33@gmail.com>
2024-01-17 23:01:33 +0100

    Ajoutez des fichiers projet.

---
+++ SynetraMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SynetraMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SynetraMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SynetraMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
